dc1011b1d0506f78d6270b623fc1de9059d80aed
zukam fodouop <zukam.fp@outlook.com>
2024-02-20 13:45:53 +0100

    Task manager maj

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "task#index"
+  get "tasks/new", to: "task#new"
+  get "tasks/:id", to: "task#show", as: "task"
+  post "tasks", to: "task#create", as: "new"
+  get "tasks/:id/edit", to: "task#edit", as: "edit_task"
+  patch "tasks/:id", to: "task#update"
+  delete "tasks/:id", to: "task#destroy"
 
   # Defines the root path route ("/")
   # root "posts#index"
